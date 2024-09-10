22201c9641be9a0f1a0e76e71bc036da1608854c
Ashutosh Soni <ashutosh10020@gmail.com>
2024-09-10 12:39:51 +0530

    added components

--- Basic/App.js
+++ Basic/App.js
@@ -1,9 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Legacy way to create an element in React
 const heading = React.createElement("h1", {id : "heading"}, "Hello World from React !");
 const parent = React.createElement("div", {id: "parent"}, 
     React.createElement("div", {id: "child"},
      React.createElement("h1", {}, "I'm an H1 tag")));
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// Better way to create an element in React
+const heading2 = <h1 id="heading">Hello World from React !</h1>;
 
-root.render(parent);
+
+// React Functional Component
+const Component = () => {
+    return <h1 id="heading">Hello World from React !</h1>;
+}
+
+const HeadingComponent = () => (
+    <>
+    {/* This is how we can use component inside component */}
+    <Component/>
+    <h1>Hello from Component !</h1>;
+    </>
+);
+
+// same can also be written in this way if it has single line
+// const HeadingComponent = () => <h1>Hello from Component</h1>;
+
+// How to render this component ?
+//root.render(<HeadingComponent/>);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);
+//root.render(parent);
