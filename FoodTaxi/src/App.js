d1b690482994646cbb65bac5723b399e2a702f80
Ashutosh Soni <ashutosh10020@gmail.com>
2024-09-16 12:53:26 +0530

    added about and contact components

--- FoodTaxi/src/App.js
+++ FoodTaxi/src/App.js
@@ -2,22 +2,10 @@ import React  from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
-/**
- * Header
- *  - Logo
- *  - Nav Items
- * Body
- *  - Search
- *  - Restaurant Container
- *      - Restaurant Card
- * Footer
- *  - Copyright
- *  - Links
- *  - Address
- *  - Contact
- */
-
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const oneResData = {
     "info": {
@@ -50,5 +38,21 @@ const AppLayout = () => (
     </div>
 );
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout/>,
+    errorElement: <Error/>
+  },
+  {
+    path: "/about",
+    element: <About/>
+  },
+  {
+    path: "/contact",
+    element: <Contact/>
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>);
+root.render(<RouterProvider router={appRouter}/>);
